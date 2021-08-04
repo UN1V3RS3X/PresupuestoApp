9ecfafd50fd10db10e9f61220dc0315604af54d1
Yamid Patiño <yamidandres25@gmail.com>
2021-08-03 22:29:17 -0500

    Se agrega componente condicional y error form

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,12 +1,47 @@
-const Pregunta = () => {
+import { useState } from 'react';
+import Error from './Error';
+
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+    
+    //Definir el state
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [error, guardarError] = useState(false);
+    //Funcion que lee el presupuesto
+
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10));
+    }
+
+    // Submit para definir presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //Validar
+        if(cantidad < 1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
+
+        //Que hacer si pasa la validacion
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+
+    }
+
     return (
         <>
             <h2>Coloca tu presupuesto</h2>
-            <form>
+            {error ? <Error mensaje="El Presupuesto es Incorrecto"/>: null}
+            <form
+                onSubmit = {agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
                 <input
                     type="submit"
